9abbd1053dffcc53e1ce146a5b8028a3bc43058d
Neo Temores <neo@Mac.lan1>
2022-08-11 11:37:57 -0700

    deploy

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel'
 
+import drawingApp from '../Assets/videos/drawingDemoTrim.mov'
+import drawingAppThumbnail from '../Assets/thumbnails/drawingAppThumbnail.png'
 import babyGame from '../Assets/videos/GuessBabyNameGame.mov'
 import babyGameThumbNail from '../Assets/thumbnails/babyName.png'
 import findLuvVid from '../Assets/videos/FullChatDemo.mov'
@@ -10,6 +12,11 @@ import projectRitter from '../Assets/videos/ProjectRitterDemo.mov'
 import projectRitterThumbnail from '../Assets/thumbnails/projectRitterThumbNail.png'
 import teamGenerator from '../Assets/videos/random-team-gen-demo.mov'
 import teamGeneratorThumbnail from '../Assets/thumbnails/reandom-team-gen-thumbnail.png'
+import rubiksCube from '../Assets/videos/RubiksCubeDemo.mov'
+import rubiksCubeThumbnail from '../Assets/thumbnails/rubiksCubeThumbnail.png'
+import pokeDex from '../Assets/videos/pokeDexDemo.mov'
+import pokeDexThumbnail from '../Assets/thumbnails/pokeDexThumbnail.png'
+
 
 
 
@@ -21,32 +28,49 @@ const Portfolio = () => {
             let videos = document.querySelectorAll('video')
             videos.forEach(elem => elem.pause())
         }
+    }, [])
 
-    })
-
+    const handleClick = () => {
+        console.log("clicked")
+    }
 
     return (
         <div className='carouselMainContainer' >
             <Carousel showArrows={true} >
 
-                <video style={{ 'maxWidth': "90%", 'height': '90%' }} autoPlay controls muted>
+                <video style={{ 'maxWidth': "90%", 'height': '90%' }} loop autoPlay controls muted>
                     <source src={babyGame} type='video/mp4' />
-                    <img src={babyGameThumbNail} height="50" alt='pic' />
+                    <img src={babyGameThumbNail} onClick={handleClick} height="50" alt='pic' />
                 </video>
 
-                <video style={{ 'maxWidth': "90%", 'height': '90%' }} autoPlay controls muted>
+                <video style={{ 'maxWidth': "90%", 'height': '90%' }} loop autoPlay controls muted>
                     <source src={findLuvVid} type='video/mp4' />
                     <img src={findLuvVivThumbNail} height="50" alt='pic' />
                 </video>
 
-                <video style={{ 'maxWidth': "90%", 'height': '90%' }} autoPlay controls muted>
+                <video style={{ 'maxWidth': "90%", 'height': '90%' }} loop autoPlay controls muted>
                     <source src={projectRitter} type='video/mp4' />
                     <img src={projectRitterThumbnail} height="50" alt='pic' />
                 </video>
 
-                <video style={{ 'maxWidth': "90%", 'height': '90%' }} autoPlay controls muted>
+                <video style={{ 'maxWidth': "90%", 'height': '90%' }} loop autoPlay controls muted>
                     <source src={teamGenerator} type='video/mp4' />
                     <img src={teamGeneratorThumbnail} height="50" alt='pic' />
+                </video>
+
+                <video style={{ 'maxWidth': "90%", 'height': '90%' }} loop autoPlay controls muted>
+                    <source src={rubiksCube} type='video/mp4' />
+                    <img src={rubiksCubeThumbnail} height="50" alt='pic' />
+                </video>
+
+                <video style={{ 'maxWidth': "90%", 'height': '90%' }} loop autoPlay controls muted >
+                    <source src={drawingApp} type='video/mp4' />
+                    <img src={drawingAppThumbnail} height="50" alt='pic' />
+                </video>
+
+                <video style={{ 'maxWidth': "90%", 'height': '90%' }} loop autoPlay controls muted >
+                    <source src={pokeDex} type='video/mp4' />
+                    <img src={pokeDexThumbnail} height="50" alt='pic' />
                 </video>
 
             </Carousel>
