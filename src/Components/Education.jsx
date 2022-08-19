d06bd154e3d3c74ac47c807021f84e336001183d
Neo Temores <Neo@Neos-Air.lan1>
2022-08-19 14:08:27 -0700

    aligning education info

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const Education = () => {
-
-    const createEducationDiv = ()=>{
-        
-    }
+  const createEducationDiv = (schoolName, program, gradDate, description) => {
     return (
-        <div className='educationMainContainer'>
-            <p>--My Education history will go here!</p>
-            <div>
-                <h2></h2>
-            </div>
+      <>
+        <div className="educationProgramSchool">
+          <h3>{program}</h3> <h4>{schoolName}</h4>
+          <h6 className="gradDate">Graduation date: {gradDate}</h6>
         </div>
-    )
-}
+        <p>{description}</p>
+      </>
+    );
+  };
+  return (
+    <div className="educationMainContainer">
+      {createEducationDiv(
+        "Galvanize Operation Level-Up",
+        "Full stack software engineering immersive",
+        "Aug 2022",
+        "18-week software engineering immersive program with over 1000+ hours of coding. Full-Stack development with JavaScript, React, HTML, CSS, Express, Node and PostgreSQL in an AGILE environment "
+      )}
+      {createEducationDiv(
+        "DeVry University",
+        "B.S. Computer Information Systems, Software Development",
+        "Expected June 2025",
+        "Completed 30 credits towards a BS in Computer Information Systems, concentration in Software Programming  "
+      )}
+    </div>
+  );
+};
 
-export default Education
+export default Education;
