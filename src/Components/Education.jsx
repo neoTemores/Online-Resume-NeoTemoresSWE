8de679550cad85375f49cba09713359b65eff22c
Neo Temores <Neo@Neos-Air.lan1>
2022-08-19 13:30:40 -0700

    starting education exp

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
 const Education = () => {
+
+    const createEducationDiv = ()=>{
+        
+    }
     return (
-        <div>
+        <div className='educationMainContainer'>
             <p>--My Education history will go here!</p>
+            <div>
+                <h2></h2>
+            </div>
         </div>
     )
 }
