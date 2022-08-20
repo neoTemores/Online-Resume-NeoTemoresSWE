d9f37d53fd907ca5be7e1aedffc8bf9cd2776626
Neo Temores <Neo@Neos-Air.lan1>
2022-08-19 20:23:39 -0700

    conditional render of education name

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { GoPrimitiveDot } from "react-icons/go";
 
 const Education = () => {
-  const createEducationDiv = (schoolName, program, gradDate, description) => {
+  const [windowSize, setWindowSize] = useState(null);
+
+  useEffect(() => {
+    setWindowSize(window.innerWidth);
+    window.addEventListener("resize", () => {
+      setWindowSize(window.innerWidth);
+    });
+  }, []);
+  const createEducationDiv = (
+    schoolName,
+    program,
+    mobileProgram,
+    gradDate,
+    description
+  ) => {
     return (
-      <div>
-        <div className="educationProgramSchool">
-          <h3>{program}</h3> <h4>{schoolName}</h4>
-          <h6 className="gradDate">Graduation date: {gradDate}</h6>
-        </div>
-        <p className="educationDescription">{description}</p>
+      <div className="educationDiv">
+        <ul className="educationUL">
+          <li>
+            <h3 className="edProgram">
+              {windowSize > 600 ? program : mobileProgram}
+            </h3>
+          </li>
+          <li>
+            <h4 className="schoolName">{schoolName}</h4>
+          </li>
+          <li>
+            <h5 className="gradDate">Graduation date: {gradDate}</h5>
+          </li>
+        </ul>
+
+        <p className="educationDescription">
+          <GoPrimitiveDot />
+          {description}
+        </p>
       </div>
     );
   };
@@ -17,12 +45,14 @@ const Education = () => {
       {createEducationDiv(
         "Galvanize Operation Level-Up",
         "Full stack software engineering immersive",
+        "Full stack software engineering immersive",
         "Aug 2022",
         "18-week software engineering immersive program with over 1000+ hours of coding. Full-Stack development with JavaScript, React, HTML, CSS, Express, Node and PostgreSQL in an AGILE environment "
       )}
       {createEducationDiv(
         "DeVry University",
         "B.S. Computer Information Systems, Software Development",
+        "B.S. CIS, Software Development",
         "Expected June 2025",
         "Completed 30 credits towards a BS in Computer Information Systems, concentration in Software Programming  "
       )}
