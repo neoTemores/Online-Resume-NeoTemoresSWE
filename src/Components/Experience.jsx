7405c4edb63e50a7566185b319c536ae88aebd1b
Neo Temores <Neo@Neos-Air.lan1>
2022-08-19 21:40:28 -0700

    working on work exp

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
 
 const Experience = () => {
+  const createWorkExp = (title, company, startDate, endDate) => {
     return (
-        <div>
-            <p>--My work experience will go here!</p>
-        </div>
-    )
-}
+      <div>
+        <h3>{title}</h3>
+        <h4>{company}</h4>
+        <h5>{startDate}</h5>
+        <h5>{endDate}</h5>
+      </div>
+    );
+  };
+  return (
+    <div className="mainExperienceContainer">
+      {createWorkExp(
+        "Cavalry Scout, Sergeant",
+        "U.S. Army",
+        "Apr 2019",
+        "Aug 2022"
+      )}
+      {createWorkExp(
+        "Service Advisor",
+        "Berts Mega Mall service dept.",
+        "June 2015",
+        "Apr 2019"
+      )}
+    </div>
+  );
+};
 
-export default Experience
+export default Experience;
