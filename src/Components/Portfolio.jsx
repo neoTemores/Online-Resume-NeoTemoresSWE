0b3ff16ace3445696c6431663c1aa2b9d8fa4350
Neo Temores <Neo@Neos-Air.lan1>
2022-08-24 10:30:57 -0700

    working on video descriptions

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -23,34 +23,80 @@ const Portfolio = ({ innerWidth }) => {
     const displayDescription = () => {
         switch (currentSlide) {
             case 0:
-                return 'Guess baby name game'
-                break;
+                return (
+                    <div className='videoDescription'>
+                        <h1>Wheel of Fortune game</h1>
+                        <p>I built this application because my wife and I wanted to play a "guess the baby name" game at her babyshower. The game was a huge success and our family had a great time playing!</p>
+                        <p>This app was built using JavaScript React. Each tile is rendered using a reusable 'tile' component. I used the 'onClick' attribute and 'useState' hook to dynamically change the blank tile to display the letter when clicked on.</p>
+                    </div>
+                )
             case 1:
-                return 'Find luv chat demo'
-                break;
+                return (
+                    <div className='videoDescription'>
+                        <h1>FindLuv Dating app</h1>
+                        <p>This application was built as part of a school project. A user is able to create an account, log in and search for other users based on dating match preferences that are set within the users profile page.</p>
+                        <p>This video is demonstrating the chat feature I created. The user has an inbox that contains all threads. When a thread is clicked on, the chat modal appears. The relational  SQL database includes a users table, a messages table, and a threads join table. Each time the user clicks 'send msg', the database is updated with the msg content, sender id and receipent id. The react 'useEffect' hook is used to pull new msgs from the database.</p>
+                    </div>
+                )
             case 2:
-                return 'Project ritter a social media twitter clone'
-                break;
+                return (
+                    <div className='videoDescription'>
+                        <h1>Project Ritter Micro Blog app</h1>
+                        <p>This application is dedicated to the memory of SPC Hunter Ritter. He was a Soldier, a fellow software engineer student and my friend.</p>
+                        <p>This video demonstration walks through some of the application features. A user can create an account, log in, view all posts from all users, click on a post to see posts from only that individual user, edit / delete posts made by the loged in user, change profile settings along with the option to toggle a dark theme. </p>
+                        <p>I used JavaScript React Router to create the multiple pages. The navbar contains the react router 'Link' component to navigate between pages on the client side. The SQL database contains a users table and a posts table. The API was built with full CRUD (create, read, update, delete) functionality for the user and posts.</p>
+                    </div>
+                )
             case 3:
-                return 'A 3D interactive Rubick\'s cube made with Three.js'
-                break;
+                return (
+                    <div className='videoDescription'>
+                        <h1>3D Interactive Rubik's Cube</h1>
+                        <p>As part of a school project, this was my first attempt at working with the JavaScript Three.js library.I decided to build a Rubik's cube because not only do i enjoy solving them for fun, but I believed it would be a way to challenge myself.</p>
+                        <p>The user is able to manipulate the cube in multiple ways. You are able to spin and rotate it by clicking and dragging the cube in the desired direction. I used 'addEventListener' to bind the movement of certain faces of the cube to specific keys.</p>
+                    </div>
+                )
             case 4:
-                return 'Random team generator made as a side project'
-                break;
+                return (
+                    <div className='videoDescription'>
+                        <h1>Random Team Generator app</h1>
+                        <p>I decided to build this application as a side project while attending Galvanize software immerseive program. At the time, our instructor had to manually asign each student to a team. I noticed that this process was a little time consuming, and so I took the initiative to create an application that would auto generate teams for our class.</p>
+                        <p>The user is able to select the team size desired, and then deselect any student to omit their name from being assigned to a team. This application was built using regular vanilla JavaScript. The list on names are contained within an array. The array is randomized and then mapped over to sort the names into teams whenever the 'Generate Teams' button is clicked.</p>
+                    </div>
+                )
             case 5:
-                return 'Pokemon card search application'
-                break;
+                return (
+                    <div className='videoDescription'>
+                        <h1>title</h1>
+                        <p>p1</p>
+                        <p>p2</p>
+                    </div>
+                )
             case 6:
-                return 'Pixel art drawing app'
-                break;
+                return (
+                    <div className='videoDescription'>
+                        <h1>title</h1>
+                        <p>p1</p>
+                        <p>p2</p>
+                    </div>
+                )
             case 7:
-                return 'Ebay page replica'
-                break;
+                return (
+                    <div className='videoDescription'>
+                        <h1>title</h1>
+                        <p>p1</p>
+                        <p>p2</p>
+                    </div>
+                )
             case 8:
-                return 'TV show search app'
-                break;
+                return (
+                    <div className='videoDescription'>
+                        <h1>title</h1>
+                        <p>p1</p>
+                        <p>p2</p>
+                    </div>
+                )
             default:
-                return ''
+                return (<h1>Error loading video description.</h1>)
         }
     }
 
@@ -94,7 +140,7 @@ const Portfolio = ({ innerWidth }) => {
                     <img src={tvGuideThumbnail} height="50" alt='pic' />
                 </iframe>
             </Carousel>
-            <p>{displayDescription()}</p>
+            <>{displayDescription()}</>
         </div>
 
 
